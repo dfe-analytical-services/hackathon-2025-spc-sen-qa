ea1967021afd9e00c195d635953b2323fc68817e
JBrett <josie.brett@education.gov.uk>
2025-09-03 19:59:22 +0100

    rough work for fsm_eth_lang

--- analysis/04_SEN_totals.R
+++ analysis/04_SEN_totals.R
@@ -26,3 +26,24 @@ sen_age_sex_long <- sen_age_sex %>%
 test <- long_df %>% 
   filter(time_period == 202324, sen_status == 'Total', sen_primary_need == 'Total', geographic_level == 'National', phase_type_grouping == 'Total') 
 View(test)
+
+# rough work for ethnicity
+
+library(tidyverse)
+
+# Select relevant columns
+cols_to_pivot <- names(sen_fsm_eth_lang)[str_detect(names(sen_fsm_eth_lang), "^(fsm|ethnicity|language)_") & 
+                             !str_detect(names(sen_fsm_eth_lang), "_percent$")]
+
+# Pivot longer
+sen_fsm_eth_lang_long <- sen_fsm_eth_lang %>%
+  pivot_longer(
+    cols = all_of(cols_to_pivot),
+    names_to = c("category", "sub_category"),
+    names_sep = "_",
+    values_to = "value"
+  ) %>%
+  pivot_wider(
+    names_from = category,
+    values_from = sub_category
+  )

c8f407d47d2676630251a9bec98b4abebc050858
CHALAM-JUDGE <Nathan.CHALAM-JUDGE@EDUCATION.GOV.UK>
2025-09-03 14:06:22 +0100

    function working for subgroup - need to factor in Total

--- analysis/04_SPC_totals_Nathan.R
+++ analysis/04_SPC_totals_Nathan.R
@@ -1,5 +1,5 @@
 # Calculates totals for the SPC publication data files
-test_summarise = function(a, b, c) {
+test_summarise = function(a, b, c, d) {
 #Nathan
   
 if( b=="") {
@@ -8,37 +8,37 @@ if( b=="") {
   spc_ap_placement_totals <- a %>%
     #filter(time_period == time_identifier) %>%
     group_by(geographic_level) %>%
-    summarise(pupil_count = sum(number_of_pupils, na.rm = T))
+    summarise(count = sum(!!sym(d), na.rm = T))
   
   spc_ap_placement_totals_regions <- a %>%
     #filter(time_period == time_identifier) %>%
     filter(region_name != "",la_name != "") %>%
     group_by(region_name) %>%
-    summarise(pupil_count = sum(number_of_pupils, na.rm = T)) %>%
-    summarise(region_total = sum(pupil_count)) %>%
+    summarise(count = sum(!!sym(d), na.rm = T)) %>%
+    summarise(region_total = sum(count)) %>%
     mutate(geographic_level = "Regional")
   
   spc_ap_placement_totals_la <- a %>%
     #filter(time_period == time_identifier) %>%
     filter(region_name != "",la_name != "") %>%
     group_by(la_name) %>%
-    summarise(pupil_count = sum(number_of_pupils, na.rm = T)) %>%
-    summarise(la_total = sum(pupil_count)) %>%
+    summarise(count = sum(!!sym(d), na.rm = T)) %>%
+    summarise(la_total = sum(count)) %>%
     mutate(geographic_level = "Local authority")
   
   spc_ap_placement_totals_region_ind <- a %>%
     #filter(time_period == time_identifier) %>%
     filter(region_name != "", la_name != "") %>%
     group_by(region_name) %>%
-    summarise(pupil_count = sum(number_of_pupils, na.rm = T))
+    summarise(count = sum(!!sym(d), na.rm = T))
   
   spc_ap_placement_totals_la_ind <- a %>%
     #filter(time_period == time_identifier) %>%
     filter(region_name != "", la_name != "") %>%
     group_by(region_name,la_name) %>%
-    summarise(pupil_count = sum(number_of_pupils, na.rm = T)) %>%
+    summarise(count = sum(!!sym(d), na.rm = T)) %>%
     group_by(region_name) %>%
-    summarise(la_region_total = sum(pupil_count))
+    summarise(la_region_total = sum(count))
   
   spc_ap_placement_totals_region_comparison <- spc_ap_placement_totals_region_ind %>%
     left_join(spc_ap_placement_totals_la_ind, by = c("region_name"))
@@ -55,47 +55,47 @@ if( b=="") {
 spc_ap_placement_totals <- a %>%
   #filter(time_period == time_identifier) %>%
   group_by(geographic_level, !!sym(b)) %>%
-  summarise(pupil_count = sum(number_of_pupils, na.rm = T))
+  summarise(count = sum(!!sym(d), na.rm = T))
 
 spc_ap_placement_totals_regions <- a %>%
   #filter(time_period == time_identifier) %>%
   filter(region_name != "",la_name != "") %>%
   group_by(region_name, !!sym(b)) %>%
-  summarise(pupil_count = sum(number_of_pupils, na.rm = T)) %>%
+  summarise(count = sum(!!sym(d), na.rm = T)) %>%
   group_by(!!sym(b)) %>%
-  summarise(region_total = sum(pupil_count)) %>%
+  summarise(region_total = sum(count)) %>%
   mutate(geographic_level = "Regional")
 
 spc_ap_placement_totals_la <- a %>%
   #filter(time_period == time_identifier) %>%
   filter(region_name != "",la_name != "") %>%
   group_by(la_name, !!sym(b)) %>%
-  summarise(pupil_count = sum(number_of_pupils, na.rm = T)) %>%
+  summarise(count = sum(!!sym(d), na.rm = T)) %>%
   group_by(!!sym(b)) %>%
-  summarise(la_total = sum(pupil_count)) %>%
+  summarise(la_total = sum(count)) %>%
   mutate(geographic_level = "Local authority")
 
 spc_ap_placement_totals_region_ind <- a %>%
   #filter(time_period == time_identifier) %>%
   filter(region_name != "", la_name != "") %>%
   group_by(region_name, !!sym(b)) %>%
-  summarise(pupil_count = sum(number_of_pupils, na.rm = T))
+  summarise(count = sum(!!sym(d), na.rm = T))
 
 spc_ap_placement_totals_la_ind <- a %>%
   #filter(time_period == time_identifier) %>%
   filter(region_name != "", la_name != "") %>%
   group_by(region_name,la_name, !!sym(b)) %>%
-  summarise(pupil_count = sum(number_of_pupils, na.rm = T)) %>%
+  summarise(count = sum(!!sym(d), na.rm = T)) %>%
   group_by(region_name, !!sym(b)) %>%
-  summarise(la_region_total = sum(pupil_count))
+  summarise(la_region_total = sum(count))
 
 spc_ap_placement_totals_region_comparison <- spc_ap_placement_totals_region_ind %>%
-  left_join(spc_ap_placement_totals_la_ind, by = c("region_name", as.character(!!sym(b))))
+  left_join(spc_ap_placement_totals_la_ind, by = c("region_name", b))
   
 
 spc_ap_placement_totals_national_comparison <- spc_ap_placement_totals %>%
-  left_join(spc_ap_placement_totals_regions, by = c("geographic_level", as.character(!!sym(b)))) %>%
-  left_join(spc_ap_placement_totals_la, by = c("geographic_level", as.character(!!sym(b))))
+  left_join(spc_ap_placement_totals_regions, by = c("geographic_level", b)) %>%
+  left_join(spc_ap_placement_totals_la, by = c("geographic_level", b))
 
 }
 
@@ -105,10 +105,16 @@ assign(paste0("national_comparison", c), spc_ap_placement_totals_national_compar
 
 }
 
-test_summarise(spc_ap_placement,"", "ap_placement_total")
-test_summarise(spc_ap_placement, "sex", "ap_placement_sex")
+#
+test_summarise(spc_ap_placement,"", "ap_placement_total", "number_of_pupils")
+test_summarise(spc_ap_placement, "sex", "ap_placement_sex", "number_of_pupils")
+test_summarise(spc_ap_placement, "age", "ap_placement_age", "number_of_pupils")
+test_summarise(spc_ap_placement, "fsm", "ap_placement_fsm", "number_of_pupils")
+test_summarise(spc_ap_placement, "ethnicity_minor", "ap_placement_ethnicity", "number_of_pupils")
 
-
+test <- spc_ap_placement %>%
+  group_by(sex) %>%
+  summarise(count = n() )
 #Free school meals - ethnitiy & year group
 spc_pupil_fsm 
 
@@ -132,38 +138,38 @@ spc_school_chars
 spc_ap_placement_totals <- spc_ap_placement %>%
   #filter(time_period == time_identifier) %>%
   group_by(geographic_level, sex) %>%
-  summarise(pupil_count = sum(number_of_pupils, na.rm = T))
+  summarise(count = sum(!!sym(d), na.rm = T))
 
 spc_ap_placement_totals_regions <- spc_ap_placement %>%
   #filter(time_period == time_identifier) %>%
   filter(region_name != "",la_name != "") %>%
   group_by(region_name, sex) %>%
-  summarise(pupil_count = sum(number_of_pupils, na.rm = T)) %>%
-  summarise(region_total = sum(pupil_count)) %>%
+  summarise(count = sum(!!sym(d), na.rm = T)) %>%
+  summarise(region_total = sum(count)) %>%
   mutate(geographic_level = "Regional")
 
 spc_ap_placement_totals_la <- spc_ap_placement %>%
   #filter(time_period == time_identifier) %>%
   filter(region_name != "",la_name != "") %>%
   group_by(la_name, sex) %>%
-  summarise(pupil_count = sum(number_of_pupils, na.rm = T)) %>%
+  summarise(count = sum(!!sym(d), na.rm = T)) %>%
 
-  summarise(la_total = sum(pupil_count)) %>%
+  summarise(la_total = sum(count)) %>%
   mutate(geographic_level = "Local authority")
 
 spc_ap_placement_totals_region_ind <- spc_ap_placement %>%
   #filter(time_period == time_identifier) %>%
   filter(region_name != "", la_name != "") %>%
   group_by(region_name, sex) %>%
-  summarise(pupil_count = sum(number_of_pupils, na.rm = T))
+  summarise(count = sum(!!sym(d), na.rm = T))
 
 spc_ap_placement_totals_la_ind <- spc_ap_placement %>%
   #filter(time_period == time_identifier) %>%
   filter(region_name != "", la_name != "") %>%
   group_by(region_name,la_name, sex) %>%
-  summarise(pupil_count = sum(number_of_pupils, na.rm = T)) %>%
+  summarise(count = sum(!!sym(d), na.rm = T)) %>%
   group_by(region_name, sex) %>%
-  summarise(la_region_total = sum(pupil_count))
+  summarise(la_region_total = sum(count))
 
 spc_ap_placement_totals_region_comparison <- spc_ap_placement_totals_region_ind %>%
   left_join(spc_ap_placement_totals_la_ind, by = c("region_name", "sex"))

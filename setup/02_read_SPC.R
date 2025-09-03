7a652452716acbed68fad06929c306ef789ef61b
CHALAM-JUDGE <Nathan.CHALAM-JUDGE@EDUCATION.GOV.UK>
2025-09-03 11:54:40 +0100

    splits SPC read in between Mark and Nathan

---
+++ setup/02_read_SPC.R
@@ -0,0 +1,19 @@
+# Reads in the SPC publication data files
+
+#Mark
+#spc_pupils_age_and_sex.csv
+#spc_pupils_yeargroup_and_sex.csv
+#spc_pupils_yeargroup_and_sex.csv
+#spc_pupils_fsm.csv
+#spc_class_size.csv
+#spc_ap_setting.csv
+#ap_placement_reason_ap_census.csv
+
+#Nathan
+#ap_placements_ap_census.csv
+#spc_pupils_fsm_ethnicity_yrgp.csv
+#spc_uifsm.csv
+#spc_pupils_young_carers_yrgp.csv
+#spc_school_ap_characteristics.csv
+#spc_school_ap_placement.csv
+#spc_school_characteristics.csv

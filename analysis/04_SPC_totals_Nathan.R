a8376fed08ad55e156f1e3581e249e3ec6829d0e
CHALAM-JUDGE <Nathan.CHALAM-JUDGE@EDUCATION.GOV.UK>
2025-09-04 09:22:16 +0100

    update to function

--- analysis/04_SPC_totals_Nathan.R
+++ analysis/04_SPC_totals_Nathan.R
@@ -6,118 +6,165 @@ if( b=="") {
 
   
   #Alternative provision placements
-  geographic_totals <- a %>%
-    filter(time_period == !!time_identifier) %>%
-    filter(if_all(e, ~ .x == "Total")) %>%
-    group_by(geographic_level) %>%
-    summarise(count = sum(!!sym(d), na.rm = T))
   
+  national_totals <- a %>%
+    filter(time_period == !!time_identifier) %>%
+    filter(geographic_level == "National") %>%
+    filter(if_all(e, ~ .x == "Total")) %>%
+    summarise(national_total = sum(!!sym(d), na.rm = T))
+    
   regional_totals <- a %>%
     filter(time_period == !!time_identifier) %>%
-    filter(region_name != "",la_name != "") %>%
+    filter(geographic_level == "Regional") %>%
     filter(if_all(e, ~ .x == "Total")) %>%
     group_by(region_name) %>%
-    summarise(count = sum(!!sym(d), na.rm = T)) %>%
-    summarise(region_total = sum(count)) %>%
-    mutate(geographic_level = "Regional")
+    summarise(region_total = sum(!!sym(d), na.rm = T))
   
   la_totals <- a %>%
     filter(time_period == !!time_identifier) %>%
-    filter(region_name != "",la_name != "") %>%
+    filter(geographic_level == "Local authority") %>%
     filter(if_all(e, ~ .x == "Total")) %>%
     group_by(la_name) %>%
-    summarise(count = sum(!!sym(d), na.rm = T)) %>%
-    summarise(la_total = sum(count)) %>%
-    mutate(geographic_level = "Local authority")
+    summarise(la_total = sum(!!sym(d), na.rm = T))
   
-  individual_region_totals <- a %>%
+  la_regional_totals <- a %>%
     filter(time_period == !!time_identifier) %>%
-    filter(region_name != "", la_name != "") %>%
+    filter(geographic_level == "Local authority") %>%
     filter(if_all(e, ~ .x == "Total")) %>%
     group_by(region_name) %>%
-    summarise(count = sum(!!sym(d), na.rm = T))
+    summarise(la_total = sum(!!sym(d), na.rm = T))
   
-  individual_la_totals <- a %>%
+  #checks all geographic breakdowns sum to country
+  all_nation_totals <- a %>%
     filter(time_period == !!time_identifier) %>%
-    filter(region_name != "", la_name != "") %>%
     filter(if_all(e, ~ .x == "Total")) %>%
-    group_by(region_name,la_name) %>%
+    group_by(geographic_level) %>%
     summarise(count = sum(!!sym(d), na.rm = T)) %>%
-    group_by(region_name) %>%
-    summarise(la_region_total = sum(count))
+    pivot_wider(names_from = geographic_level, values_from = count) %>%
+    mutate(geographic_level = "National")
   
-  region_comparison_totals <- individual_region_totals %>%
-    left_join(individual_la_totals, by = c("region_name"))
+  #checks regional and la breakdowns sum to region
+  regional_totals_comparison <- regional_totals %>%
+    left_join(la_regional_totals, by = "region_name")
   
+  # 
+  # 
+  # individual_region_totals <- a %>%
+  #   filter(time_period == !!time_identifier) %>%
+  #   filter(region_name != "", la_name != "") %>%
+  #   filter(if_all(e, ~ .x == "Total")) %>%
+  #   group_by(region_name) %>%
+  #   summarise(count = sum(!!sym(d), na.rm = T))
+  # 
+  # individual_la_totals <- a %>%
+  #   filter(time_period == !!time_identifier) %>%
+  #   filter(region_name != "", la_name != "") %>%
+  #   filter(if_all(e, ~ .x == "Total")) %>%
+  #   group_by(region_name,la_name) %>%
+  #   summarise(count = sum(!!sym(d), na.rm = T)) %>%
+  #   group_by(region_name) %>%
+  #   summarise(la_region_total = sum(count))
   
-  national_comparison_totals <- geographic_totals %>%
-    left_join(regional_totals, by = "geographic_level") %>%
-    left_join(la_totals, by = "geographic_level")
-  
+  # region_comparison_totals <- individual_region_totals %>%
+  #   left_join(individual_la_totals, by = c("region_name"))
+  # 
+  # 
+  # national_comparison_totals <- geographic_totals %>%
+  #   left_join(regional_totals, by = "geographic_level") %>%
+  #   left_join(la_totals, by = "geographic_level")
+
 }
   else {
   
 #Alternative provision placements
-geographic_totals <- a %>%
+national_totals <- a %>%
   filter(time_period == !!time_identifier) %>%
-  group_by(geographic_level, !!sym(b)) %>%
-  filter(b !="Total") %>%
+  filter(geographic_level == "National") %>%
+  filter(!!sym(b) !="Total") %>%
   filter(if_all(e, ~ .x == "Total")) %>%
-  summarise(count = sum(!!sym(d), na.rm = T))
+  group_by(!!sym(b)) %>%
+  summarise(national_total = sum(!!sym(d), na.rm = T))
 
 regional_totals <- a %>%
   filter(time_period == !!time_identifier) %>%
-  filter(region_name != "",la_name != "") %>%
+  filter(geographic_level == "Regional") %>%
   filter(if_all(e, ~ .x == "Total"),
-         b != "Total") %>%
+         !!sym(b) != "Total") %>%
   group_by(region_name, !!sym(b)) %>%
-  summarise(count = sum(!!sym(d), na.rm = T)) %>%
-  group_by(!!sym(b)) %>%
-  summarise(region_total = sum(count)) %>%
-  mutate(geographic_level = "Regional")
+  summarise(region_total = sum(!!sym(d), na.rm = T))
 
 la_totals <- a %>%
   filter(time_period == !!time_identifier) %>%
-  filter(region_name != "",la_name != "") %>%
+  filter(geographic_level == "Local authority") %>%
   filter(if_all(e, ~ .x == "Total"),
-         b != "Total") %>%
+         !!sym(b) != "Total") %>%
   group_by(la_name, !!sym(b)) %>%
-  summarise(count = sum(!!sym(d), na.rm = T)) %>%
-  group_by(!!sym(b)) %>%
-  summarise(la_total = sum(count)) %>%
-  mutate(geographic_level = "Local authority")
+  summarise(la_total = sum(!!sym(d), na.rm = T))
 
-individual_region_totals <- a %>%
+la_regional_totals <- a %>%
   filter(time_period == !!time_identifier) %>%
-  filter(region_name != "", la_name != "") %>%
+  filter(geographic_level == "Local authority") %>%
   filter(if_all(e, ~ .x == "Total"),
-         b != "Total") %>%
+         !!sym(b) != "Total") %>%
   group_by(region_name, !!sym(b)) %>%
-  summarise(count = sum(!!sym(d), na.rm = T))
+  summarise(la_total = sum(!!sym(d), na.rm = T))
 
-individual_la_totals <- a %>%
+#checks all geographic breakdowns sum to country
+all_nation_totals <- a %>%
   filter(time_period == !!time_identifier) %>%
-  filter(region_name != "", la_name != "") %>%
-  filter(if_all(e, ~ .x == "Total"),
-         b != "Total") %>%
-  group_by(region_name,la_name, !!sym(b)) %>%
+  group_by(geographic_level, !!sym(b)) %>%
+  filter(!!sym(b) !="Total") %>%
+  filter(if_all(e, ~ .x == "Total")) %>%
   summarise(count = sum(!!sym(d), na.rm = T)) %>%
-  group_by(region_name, !!sym(b)) %>%
-  summarise(la_region_total = sum(count))
+  pivot_wider(names_from = geographic_level, values_from = count) %>%
+  mutate(geographic_level = "National")
 
-region_comparison_totals <- individual_region_totals %>%
-  left_join(individual_la_totals, by = c("region_name", b))
-  
+#checks regional and la breakdowns sum to region
+regional_totals_comparison <- regional_totals %>%
+  left_join(la_regional_totals, by = c("region_name",b))
 
-national_comparison_totals <- geographic_totals %>%
-  left_join(regional_totals, by = c("geographic_level", b)) %>%
-  left_join(la_totals, by = c("geographic_level", b))
+# 
+# individual_region_totals <- a %>%
+#   filter(time_period == !!time_identifier) %>%
+#   filter(region_name != "", la_name != "") %>%
+#   filter(if_all(e, ~ .x == "Total"),
+#          b != "Total") %>%
+#   group_by(region_name, !!sym(b)) %>%
+#   summarise(count = sum(!!sym(d), na.rm = T))
+# 
+# individual_la_totals <- a %>%
+#   filter(time_period == !!time_identifier) %>%
+#   filter(region_name != "", la_name != "") %>%
+#   filter(if_all(e, ~ .x == "Total"),
+#          b != "Total") %>%
+#   group_by(region_name,la_name, !!sym(b)) %>%
+#   summarise(count = sum(!!sym(d), na.rm = T)) %>%
+#   group_by(region_name, !!sym(b)) %>%
+#   summarise(la_region_total = sum(count))
+
+
+
+
+
+# region_comparison_totals <- individual_region_totals %>%
+#   left_join(individual_la_totals, by = c("region_name", b))
+#   
+# 
+# national_comparison_totals <- geographic_totals %>%
+#   left_join(regional_totals, by = c("geographic_level", b)) %>%
+#   left_join(la_totals, by = c("geographic_level", b))
 
 }
 
-
-assign(paste0("regional_comparison_",c), region_comparison_totals, envir = .GlobalEnv)
-assign(paste0("national_comparison_", c), national_comparison_totals, envir = .GlobalEnv)
+#write out
+  #total for each breakdown across la, region, nation
+  #percentage match across la, region, nation
+  #Y/N flag for 100% match
+  assign(paste0("national_comparison_",c), all_nation_totals, envir = .GlobalEnv)
+  assign(paste0("regional_comparison_", c), regional_totals_comparison, envir = .GlobalEnv)
+  assign(paste0("national_total_", c), national_totals, envir = .GlobalEnv)
+  assign(paste0("regional_total_", c), regional_totals, envir = .GlobalEnv)
+  assign(paste0("la_total_", c), la_totals, envir = .GlobalEnv)
 
 }
 
@@ -143,56 +190,5 @@ for (i in variables) {
 }
 test_summarise(dataset, "", "pupil_fsm_total", "number_of_pupils", variables)
 
-
-
-
-#Alternative provision placements
-geographic_totals <- spc_pupil_fsm %>%
-  filter(time_period == !!time_identifier) %>%
-  filter(if_all(variables, ~ .x == "Total")) %>%
-  group_by(geographic_level) %>%
-  summarise(count = sum(number_of_pupils, na.rm = T))
-
-regional_totals <- spc_pupil_fsm %>%
-  filter(time_period == !!time_identifier) %>%
-  filter(region_name != "",la_name != "") %>%
-  filter(if_all(variables, ~ .x == "Total")) %>%
-  group_by(region_name) %>%
-  summarise(count = sum(number_of_pupils, na.rm = T)) %>%
-  summarise(region_total = sum(count)) %>%
-  mutate(geographic_level = "Regional")
-
-la_totals <- spc_pupil_fsm %>%
-  filter(time_period == !!time_identifier) %>%
-  filter(region_name != "",la_name != "") %>%
-  filter(if_all(variables, ~ .x == "Total")) %>%
-  group_by(la_name) %>%
-  summarise(count = sum(number_of_pupils, na.rm = T)) %>%
-  summarise(la_total = sum(count)) %>%
-  mutate(geographic_level = "Local authority")
-
-individual_region_totals <- spc_pupil_fsm %>%
-  filter(time_period == !!time_identifier) %>%
-  filter(region_name != "", la_name != "") %>%
-  filter(if_all(variables, ~ .x == "Total")) %>%
-  group_by(region_name) %>%
-  summarise(count = sum(number_of_pupils, na.rm = T))
-
-individual_la_totals <- spc_pupil_fsm %>%
-  filter(time_period == !!time_identifier) %>%
-  filter(region_name != "", la_name != "") %>%
-  filter(if_all(variables, ~ .x == "Total")) %>%
-  group_by(region_name,la_name) %>%
-  summarise(count = sum(number_of_pupils, na.rm = T)) %>%
-  group_by(region_name) %>%
-  summarise(la_region_total = sum(count))
-
-region_comparison_totals <- individual_region_totals %>%
-  left_join(individual_la_totals, by = c("region_name"))
-
-
-national_comparison_totals <- geographic_totals %>%
-  left_join(regional_totals, by = "geographic_level") %>%
-  left_join(la_totals, by = "geographic_level")
 
 
